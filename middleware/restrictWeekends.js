a1cb5d0db5fafbf1ed9db38224809740990a1e58
Nagendra Kumar K V <nagendrakumarkv123@gmail.com>
2025-04-26 16:03:45 +0530

    restrictWeekends middle issue resolved

--- middleware/restrictWeekends.js
+++ middleware/restrictWeekends.js
@@ -1,6 +1,7 @@
 const restrictWeekends = (req, res, next) => {
   const day = new Date().getDay(); // 0 = Sunday, 6 = Saturday
-  if (day === 0 || day === 6) {
+  // if (day === 0 || day === 6) {
+  if (day === 0) {
     return res.status(403).send("No access on weekends!");
   }
   next();
